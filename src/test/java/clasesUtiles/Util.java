a901e41d150af8da8a3d2b6a1779c0a7e73ab8a1
atelito <aaxelporetti@gmail.com>
2021-01-04 16:49:35 -0300

    adjuntoemail

--- src/test/java/clasesUtiles/Util.java
+++ src/test/java/clasesUtiles/Util.java
@@ -29,7 +29,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;	
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -41,7 +41,7 @@ public class Util {
 	private String urlBase = "";
 	private String userName = "";
 	private String password = "";
-	
+
 	// ENTORNO
 	private String nombreNavegador = "EDGE";
 	private String locationEdge = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
@@ -51,175 +51,289 @@ public class Util {
 	private DesiredCapabilities entorno;
 	private WebDriver driver;
 
-	
 	public WebDriver getDriver() {
 		return driver;
 	}
+
 	public void setDriver(WebDriver driver) {
 		this.driver = driver;
 	}
+
 	public String getNombreNavegador() {
 		return nombreNavegador;
 	}
+
 	public void setNombreNavegador(String navegador) {
 		this.nombreNavegador = navegador;
 	}
-	
+
 	public String getDriverNavegador() {
 		return driverNavegador;
 	}
+
 	public void setDriverNavegador(String driverNavegador) {
 		this.driverNavegador = driverNavegador;
 	}
-	
+
 	public String getUbicacionDriver() {
 		return ubicacionDriver;
 	}
+
 	public void setUbicacionDriver(String ubicacionDriver) {
 		this.ubicacionDriver = ubicacionDriver;
 	}
-	
+
 	public DesiredCapabilities getEntorno() {
 		return entorno;
 	}
+
 	public void setEntorno(DesiredCapabilities entorno) {
 		this.entorno = entorno;
 	}
-	
+
 	public String getVersionEdge() {
 		return versionEdge;
 	}
+
 	public void setVersionEdge(String versionEdge) {
 		this.versionEdge = versionEdge;
 	}
 
 	private int tiempoEspera = 30;
-	
-	
+
 	public int getTiempoEspera() {
 		return tiempoEspera;
 	}
+
 	public void setTiempoEspera(int tiempoEspera) {
 		this.tiempoEspera = tiempoEspera;
 	}
-		
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public String getLocationEdge() {
 		return locationEdge;
 	}
+
 	public void setLocationEdge(String locationEdge) {
 		this.locationEdge = locationEdge;
 	}
-	
+
 	public String getUrlBase() {
 		return urlBase;
 	}
+
 	public void setUrlBase(String url) {
 		this.urlBase = url;
 	}
-	
-	public Util() {}
-	
-	public static WebDriver setUpEdgeChromium(){
-			Util datos = new Util();
-			DesiredCapabilities entorno = DesiredCapabilities.edge();
-			entorno.setCapability(CapabilityType.BROWSER_NAME, datos.getNombreNavegador());
-			System.setProperty(datos.getDriverNavegador(),datos.getUbicacionDriver());
-			EdgeDriverService service = new EdgeDriverService.Builder().usingDriverExecutable(new File(datos.getUbicacionDriver())).usingAnyFreePort().build();
-			EdgeOptions opciones = new EdgeOptions();
-			//opciones.merge(entorno); MEJORAR LA UNION DE DESIRED CAPABILITIES CON EDGEOPTIONS
-			EdgeDriver driver = new EdgeDriver(service, opciones);
-			driver.manage().window().maximize();
+
+	public Util() {
+	}
+
+	public static WebDriver setUpEdgeChromium() {
+		Util datos = new Util();
+		DesiredCapabilities entorno = DesiredCapabilities.edge();
+		entorno.setCapability(CapabilityType.BROWSER_NAME, datos.getNombreNavegador());
+		System.setProperty(datos.getDriverNavegador(), datos.getUbicacionDriver());
+		EdgeDriverService service = new EdgeDriverService.Builder()
+				.usingDriverExecutable(new File(datos.getUbicacionDriver())).usingAnyFreePort().build();
+		EdgeOptions opciones = new EdgeOptions();
+		// opciones.merge(entorno); MEJORAR LA UNION DE DESIRED CAPABILITIES CON
+		// EDGEOPTIONS
+		EdgeDriver driver = new EdgeDriver(service, opciones);
+		driver.manage().window().maximize();
 		return driver;
 	}
-	
-	public static void takeScreenShot(WebDriver driver, String archivoConUbic) throws IOException{
+
+	public static void takeScreenShot(WebDriver driver, String ubicacionArchivoDestino) throws IOException {
 		TakesScreenshot capturaPant = (TakesScreenshot) driver;
 		File archivo = capturaPant.getScreenshotAs(OutputType.FILE);
-		File destino = new File(archivoConUbic);
+		File destino = new File(ubicacionArchivoDestino);
 		FileUtils.copyFile(archivo, destino);
-		
-	}
-	
 
-	public static void enviaEmail(String from, String password, String to, String subject, String cuerpoMail){
+	}
+
+	public static void enviaEmailTLS(String from, String password, String to, String asunto, String cuerpoMail) {
 		Properties propiedades = System.getProperties();
 		String host = "smtp.gmail.com";
 		propiedades.put("mail.smtp.starttls.enable", "true");
 		propiedades.put("mail.smtp.host", host);
 		propiedades.put("mail.smtp.user", from);
 		propiedades.put("mail.smtp.password", password);
-		propiedades.put("mail.smtp.port", "587"); //25 | 465 | 587
+		propiedades.put("mail.smtp.port", "587"); // 25 | 465 | 587
 		propiedades.put("mail.smtp.auth", "true");
 		Session sesion = Session.getDefaultInstance(propiedades);
 		MimeMessage mensaje = new MimeMessage(sesion);
-		
+
 		try {
 			mensaje.setFrom(new InternetAddress(from));
 			mensaje.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-			mensaje.setSubject(subject);
+			mensaje.setSubject(asunto);
 			mensaje.setText(cuerpoMail);
 			BodyPart objetoMensajeBP = new MimeBodyPart();
 			objetoMensajeBP.setText(cuerpoMail);
 			Multipart multipart = new MimeMultipart();
 			multipart.addBodyPart(objetoMensajeBP);
 			objetoMensajeBP = new MimeBodyPart();
-			
+
 			String nombreArchivo = System.getProperty("user.dir") + "\\SeleniumTestNG.pdf";
 			DataSource origen = new FileDataSource(nombreArchivo);
 			objetoMensajeBP.setDataHandler(new DataHandler(origen));
 			objetoMensajeBP.setFileName(nombreArchivo);
 			multipart.addBodyPart(objetoMensajeBP);
-			
+
 			mensaje.setContent(multipart);
 			Transport transporte = sesion.getTransport("smtp");
 			transporte.connect(host, from, to);
 			transporte.sendMessage(mensaje, mensaje.getAllRecipients());
 			transporte.close();
-		}catch (AddressException e) {
+		} catch (AddressException e) {
 			System.out.println("Problemas con la direccion de correo: " + e.getMessage());
-		}catch (MessagingException e) {
+		} catch (MessagingException e) {
 			System.out.println("No se pudo conectar al host SMTP: " + e.getMessage());
 		}
-		
-	}
-	/*
-	public static String[][] getInfoExcel(){
-		String nombreArchivo = "testData.xlsx";
-		String rutaArchivo = "D:\\Documentos\\MEGA\\PROGRAMACIÓN\\Cursos\\Selenium + Java\\Day03" + nombreArchivo;
-		String hoja = "Data";
-		String matrizDatos [][] = new String [4][2];
-		
-		int colInicio = 2, colFin = 3, filaInicio = 2, filaFin = 5;
-		
-		for (int i=colInicio; i<colFin; i++) {
-			for (int j=filaInicio; j<filaFin; j++) {
-				
-			}
-		}
-		OPCPackage pkg = OPCPackage.open(new File(nombreArchivo));
-		XSSFWorkbook excel = new XSSFWorkbook(pkg);
-		XSSFSheet hojaExcel = excel.getSheet(hoja);
-		XSSFRow row = hojaExcel.getRow(1);
 
-		
-		pkg.close();
-		
-		return matrizDatos[][];
-		
 	}
-	*/
+
+	public static void enviaEmailSSL(String from, String password, String to, String asunto, String cuerpoMail) {
+
+		// Asumiendo que estás enviando un correo electrónico a través de gmail's smtp
+		String host = "smtp.gmail.com";
+		// Obtener las propiedades del sistema
+		Properties properties = System.getProperties();
+		// Configurar el servidor de correo
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.ssl.enable", "true");
+		properties.put("mail.smtp.auth", "true");
+
+		// Obtener el objeto de la sesión y pasar el nombre de usuario y la contraseña
+		Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(from, password);
+			}
+		});
+
+		// Se utiliza para depurar problemas de SMTP
+		session.setDebug(true);
+
+		try {
+			// Crear un objeto MimeMessage por defecto.
+			MimeMessage message = new MimeMessage(session);
+			// Set From: campo de cabecera de la cabecera.
+			message.setFrom(new InternetAddress(from));
+			// Set To: campo de cabecera de la cabecera.
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			// Fijar Asunto: campo de cabecera
+			message.setSubject(asunto);
+			// Etablecer el mensaje actual...
+			message.setText(cuerpoMail);
+			System.out.println("enviando...");
+			// Envía el mensaje
+			Transport.send(message);
+			System.out.println("Mensaje enviado con éxito....");
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+			System.out.println("No se pudo conectar al host SMTP: " + mex.getMessage());
+		}
+	}
+
+	public static void enviaAdjuntoEmailSSL(String from, String password, String to, String asunto, String cuerpoMail, String path) {
+
+		// Asumiendo que estás enviando un correo electrónico a través de gmail's smtp
+		String host = "smtp.gmail.com";
+		// Obtener las propiedades del sistema
+		Properties properties = System.getProperties();
+		// Configurar el servidor de correo
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.ssl.enable", "true");
+		properties.put("mail.smtp.auth", "true");
+
+		// Obtener el objeto de la sesión y pasar el nombre de usuario y la contraseña
+		Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(from, password);
+			}
+		});
+
+		// Se utiliza para depurar problemas de SMTP
+		session.setDebug(true);
+
+		try {
+			// Crear un objeto MimeMessage por defecto.
+			MimeMessage message = new MimeMessage(session);
+			// Set From: campo de cabecera de la cabecera.
+			message.setFrom(new InternetAddress(from));
+			// Set To: campo de cabecera de la cabecera.
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			// Fijar Asunto: campo de cabecera
+			message.setSubject(asunto);
+			// Crear el objeto MimeBodyPart y establecer el texto de su mensaje
+			BodyPart msgCuerpoBP = new MimeBodyPart();
+			msgCuerpoBP.setText(cuerpoMail);
+			// Crear un nuevo objeto MimeBodyPart y poner el objeto DataHandler a este
+			// objeto
+			MimeBodyPart adjuntoBP = new MimeBodyPart();
+				// Alguno de los dos siguientes
+				//String archivo = "archivoParaEnviar.java"; // cambiar
+				String nombreArchivo = System.getProperty("user.dir") + path; // cambiar
+			
+			DataSource origen = new FileDataSource(nombreArchivo);
+			adjuntoBP.setDataHandler(new DataHandler(origen));
+			adjuntoBP.setFileName(nombreArchivo);
+			// Crear el objeto Multipart y añadir los objetos MimeBodyPart a este objeto
+			Multipart multipart = new MimeMultipart();
+			multipart.addBodyPart(msgCuerpoBP);
+			multipart.addBodyPart(adjuntoBP);
+			// Poner el objeto Multipart en el objeto de mensaje
+			message.setContent(multipart);
+			// Envía el mensaje
+			Transport.send(message);
+
+			message.setText(cuerpoMail);
+			System.out.println("enviando...");
+			System.out.println("Mensaje enviado con éxito....");
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
+	
+	/*
+	 * public static String[][] getInfoExcel(){ String nombreArchivo =
+	 * "testData.xlsx"; String rutaArchivo =
+	 * "D:\\Documentos\\MEGA\\PROGRAMACIÓN\\Cursos\\Selenium + Java\\Day03" +
+	 * nombreArchivo; String hoja = "Data"; String matrizDatos [][] = new String
+	 * [4][2];
+	 * 
+	 * int colInicio = 2, colFin = 3, filaInicio = 2, filaFin = 5;
+	 * 
+	 * for (int i=colInicio; i<colFin; i++) { for (int j=filaInicio; j<filaFin; j++)
+	 * {
+	 * 
+	 * } } OPCPackage pkg = OPCPackage.open(new File(nombreArchivo)); XSSFWorkbook
+	 * excel = new XSSFWorkbook(pkg); XSSFSheet hojaExcel = excel.getSheet(hoja);
+	 * XSSFRow row = hojaExcel.getRow(1);
+	 * 
+	 * 
+	 * pkg.close();
+	 * 
+	 * return matrizDatos[][];
+	 * 
+	 * }
+	 */
 }
